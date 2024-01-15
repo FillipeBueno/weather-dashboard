3bca93d80beec8e11b72b9d0c55fb5da4e6c2aad
Fillipe <you@example.com>
2024-01-15 20:50:13 +0000

    pushing changes, now can make second call to the API, need to work on displaying the info on the screen

--- assets/index.js
+++ assets/index.js
@@ -8,25 +8,55 @@ searchButton.addEventListener('click', presentContent);
 
 const myKey = config.MY_KEY;
 
+//FIRST API INFO:
+
 let queryUrl;
 
-let url1 = "http://api.openweathermap.org/geo/1.0/direct?q="
+const url1 = "http://api.openweathermap.org/geo/1.0/direct?q="
 
-let url2= "&limit=1&appid="
+const url2 = "&limit=1&appid="
+
+let lat;
+
+let lon;
+
+//SECOND API INFO:
+
+let queryUrl2;
+
+
 
 function presentContent() {
+
 
     queryUrl = url1 + searchInput.value + url2 + myKey;
 
     fetch(queryUrl)
-    .then(function (response) {
-        return response.json();
+        .then(function (response) {
+            return response.json();
 
-    }).then(function (data) {
-        //console.log(data);
-        //console.log(data[0].lat)
-        //console.log(data[0].lon)
+        }).then(function (data) {
+            lat = data[0].lat;
+            lon = data[0].lon
+            
+        })
+        .then(function () {
+            //console.log(lat);
+            //console.log(lon);
+
+            queryUrl2 = 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ myKey;
+            fetch(queryUrl2)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data2) {
+                    console.log(data2)
+                    let icon = data2.list[0].;
+                    console.log(icon)
+                })
+        }
+        )
 
 
-    
-});}
+
+        ;
+}
